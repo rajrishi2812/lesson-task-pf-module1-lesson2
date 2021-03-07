a40a15b057cf0e8e868d8337e487b842d420f69f
rajrishi2812 <74621652+rajrishi2812@users.noreply.github.com>
2021-03-07 14:13:54 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+ //question 1
+
+ var name="peter";
+ var age=20;
+ var isABoy=true; 
+
+  //question 2
+
+  var firstName="raj";
+  var lastName="singh";
+  var fullName="raj singh";
+
+  console.log( fullName)
+
+  //question 3
+
+  var type="frog";
+   
+  console.log(typeof "the type of frog is string")
+
+
+  //question 4
+
+  var orderHasShipped=true;
+
+  if (orderHasShipped===true) {
+      console.log("the is has shipped");
+  } else {
+       console.log("Not shipped");
+    
+  }
+
+  //question 5
+  var i = 7;
+  
+  for (i; i <= 13; i++) {
+      console.log(i);
+  }
